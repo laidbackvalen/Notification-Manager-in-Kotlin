ac18d034e4f883ba878242db3c334c8590b44ae2
laidback/valen <valen.patel1@gmail.com>
2024-02-29 17:47:28 +0530

    second commit

--- app/src/main/java/com/example/notificationmanager/MainActivity.kt
+++ app/src/main/java/com/example/notificationmanager/MainActivity.kt
@@ -1,60 +1,81 @@
 package com.example.notificationmanager
 
 import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.core.content.getSystemService
 import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     //A class that represents how a persistent notification is to be presented to the user using the NotificationManager.
+    companion object {
+        const val CHANNEL_ID : String = "My Channel" //there can be many channels, you can specify a specific channel id to work with
+        const val NOTIFIACTION_ID = 100 //there can be many channels, you can specify a specific channel id to work with
+    }
+    lateinit var notification: Notification
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Code To Convert drawable png to bitmap
-        var drawable: Drawable? =
-            ResourcesCompat.getDrawable(resources, R.drawable.ic_launcher, null)
+        var drawable: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.ic_launcher, null)
         var bitmapDrawable = drawable as BitmapDrawable
         var largeIcon: Bitmap = bitmapDrawable.bitmap
 
-        //https://developer.android.com/reference/android/app/Notification     //to know more
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE)
+        //https://www.youtube.com/watch?v=Pen_en0zhIY     //to know more
+
+        var notificationManager : NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         // NotificationManager is a class in the Android SDK that provides functionality to manage notifications shown to the user.
         // It allows you to create, update, and cancel notifications that appear in the notification drawer or as pop-up alerts.
 
-        var notification =
-            Notification.Builder(this)  //The Notification.Builder has been added to make it easier to construct Notifications.
-                //the NotificationBuilder is a class used to construct and customize notifications that are displayed to users.
-                // The NotificationBuilder is typically used in conjunction with the NotificationManager to create and manage notifications within an Android application.
+
+        if(android.os.Build.VERSION.SDK_INT>= android.os.Build.VERSION_CODES.O) { //notification channels were introduced starting from Android 8.0 (API level 26)
+             notification = Notification.Builder(this)
+                 //The Notification.Builder has been added to make it easier to construct Notifications.
+                    //the NotificationBuilder is a class used to construct and customize notifications that are displayed to users.
+                    // The NotificationBuilder is typically used in conjunction with the NotificationManager to create and manage notifications within an Android application.
+                    .setLargeIcon(Bitmap.createBitmap(largeIcon))
+                    .setSmallIcon(R.drawable.n)
+                    .setContentText("Hello People")
+                    .setSubText("Message from Valen")
+                    .setChannelId(CHANNEL_ID)
+                    // A "channel ID" in notifications typically refers to a unique identifier associated with a communication channel or endpoint through which notifications are sent or received.
+                    //it's important to note that starting from Android 8.0 (API level 26), notification channels were introduced.
+                    //Notification channels allow developers to categorize notifications and give users more control over which types of notifications they want to receive and how they are presented.
+                    //When creating notifications using the NotificationBuilder, you need to specify a notification channel for the notification to be associated with.
+                    // This ensures that the notification is properly categorized and follows the user's notification preferences.
+                    .build()
+
+            notificationManager.createNotificationChannel(NotificationChannel(CHANNEL_ID, "My notification channel", NotificationManager.IMPORTANCE_HIGH))  // NotificationManager.IMPORTANCE_HIGH is Priority or importance
+        }
+        else{
+            notification = Notification.Builder(this)
                 .setLargeIcon(Bitmap.createBitmap(largeIcon))
                 .setSmallIcon(R.drawable.n)
-                .setContentText("Hello People")
+                .setContentText("New Message")
                 .setSubText("message from Valen")
-                .setChannelId()
-                // A "channel ID" in notifications typically refers to a unique identifier associated with a communication channel or endpoint through which notifications are sent or received.
-                //it's important to note that starting from Android 8.0 (API level 26), notification channels were introduced.
-                //Notification channels allow developers to categorize notifications and give users more control over which types of notifications they want to receive and how they are presented.
-                //When creating notifications using the NotificationBuilder, you need to specify a notification channel for the notification to be associated with.
-                // This ensures that the notification is properly categorized and follows the user's notification preferences.
                 .build()
+        }
 
+        notificationManager.notify(NOTIFIACTION_ID, notification)
+        //To get different notifications form different channels make sure to specify the id related to that channel
+        //if you provide different channel id's with each notification you send you will get those notifications in different instance of notification bar
 
-        //custom class example
-        var v: Valen = Valen()
-        val b = v.a
-        Toast.makeText(this, "$b", Toast.LENGTH_SHORT).show()
+        //FOR EXAMPLE -
+        //if mansi send you a message that you receive on a channel_id1 and after that charlie send you a message
+        //your notification bar will only show charlie's message as the message will get updated as the notification are being passed on the same channel
+        //in case you want to see their notification in different notification sections you have to provide each one of them with a specific channel id
 
-    }
+        //if you want to see a mansi's message on a different notification section, provide a channel id to her chat. whenever a message gets from her side
+        //you will get a different notification section associated with her channel id
+        //all the the messages coming from that channel id will be updated and displayed on that channel id only
 
-    class Valen() { //Trial custom class
-        var a = 10
+        //same you can do with charlie to  get his messages on a different section with channel_id2
+
     }
 
     //WHAT IS CHANNEL?
